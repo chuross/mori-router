1aa7302715ee9bfcd627b1740f210a98eff5b467
chuross <azarasi.mo.mo@gmail.com>
2018-03-20 20:10:29 +0900

    add process code

--- annotation/src/main/java/com/github/chuross/morirouter/annotation/RouterPath.java
+++ annotation/src/main/java/com/github/chuross/morirouter/annotation/RouterPath.java
@@ -4,6 +4,13 @@ package com.github.chuross.morirouter.annotation;
 import com.github.chuross.morirouter.core.DefaultTransitionFactory;
 import com.github.chuross.morirouter.core.TransitionFactory;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
 public @interface RouterPath {
 
     String name();
--- compiler/src/main/kotlin/com/github/chuross/morirouter/compiler/MoriRouterProcessor.kt
+++ compiler/src/main/kotlin/com/github/chuross/morirouter/compiler/MoriRouterProcessor.kt
@@ -1,5 +1,8 @@
 package com.github.chuross.morirouter.compiler
 
+import com.github.chuross.morirouter.annotation.RouterParam
+import com.github.chuross.morirouter.annotation.RouterPath
+import com.github.chuross.morirouter.annotation.RouterPathParam
 import com.google.auto.service.AutoService
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.Filer
@@ -7,21 +10,39 @@ import javax.annotation.processing.Messager
 import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
+import javax.annotation.processing.SupportedSourceVersion
+import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
+import javax.lang.model.util.Elements
+import javax.tools.Diagnostic
 
 @AutoService(Processor::class)
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
 class MoriRouterProcessor : AbstractProcessor() {
 
     lateinit var filer: Filer
     lateinit var messager: Messager
+    lateinit var elements: Elements
 
     override fun init(processingEnv: ProcessingEnvironment) {
         super.init(processingEnv)
         filer = processingEnv.filer
         messager = processingEnv.messager
+        elements = processingEnv.elementUtils
+    }
+
+    override fun getSupportedAnnotationTypes(): MutableSet<String> {
+        return hashSetOf(
+                RouterPath::class.java.canonicalName,
+                RouterPathParam::class.java.canonicalName,
+                RouterParam::class.java.canonicalName
+        )
     }
 
     override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (annotations?.isEmpty() ?: true) {
+            messager.printMessage(Diagnostic.Kind.NOTE, "nothing annotation")
+        }
+        return false
     }
 }
