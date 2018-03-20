5b65005645f41ed2e857daab2678dbdd2efa40cf
chuross <azarasi.mo.mo@gmail.com>
2018-03-20 19:26:57 +0900

    add compiler module

---
+++ compiler/src/main/kotlin/com/github/chuross/morirouter/compiler/Processor.kt
@@ -0,0 +1,12 @@
+package com.github.chuross.morirouter.compiler
+
+import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.RoundEnvironment
+import javax.lang.model.element.TypeElement
+
+class Processor : AbstractProcessor() {
+
+    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
