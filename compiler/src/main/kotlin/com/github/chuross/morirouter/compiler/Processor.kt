c3f0a6e26cc274ff7e19b7249726e78d6e1e6ebb
chuross <azarasi.mo.mo@gmail.com>
2018-03-20 19:41:58 +0900

    add dependencies

--- compiler/src/main/kotlin/com/github/chuross/morirouter/compiler/Processor.kt
+++ compiler/src/main/kotlin/com/github/chuross/morirouter/compiler/Processor.kt
@@ -1,9 +1,12 @@
 package com.github.chuross.morirouter.compiler
 
+import com.google.auto.service.AutoService
 import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.element.TypeElement
 
+@AutoService(Processor::class)
 class Processor : AbstractProcessor() {
 
     override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
