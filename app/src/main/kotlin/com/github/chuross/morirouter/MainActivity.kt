f7a74ed5f336e4c0a08ae863b27e636f2e46749f
chuross <azarasi.mo.mo@gmail.com>
2018-03-21 04:43:03 +0900

    update sample code

--- app/src/main/kotlin/com/github/chuross/morirouter/MainActivity.kt
+++ app/src/main/kotlin/com/github/chuross/morirouter/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val router = MoriRouter(supportFragmentManager, R.id.container)
+//        router.second().launch()
     }
 }
---
+++ app/src/main/kotlin/com/github/chuross/morirouter/SecondFragment.kt
@@ -0,0 +1,11 @@
+package com.github.chuross.morirouter
+
+import android.support.v4.app.Fragment
+import com.github.chuross.morirouter.annotation.RouterPath
+
+@RouterPath(
+        name = "second",
+        uri = "morirouter://second/{id}"
+)
+class SecondFragment : Fragment() {
+}
