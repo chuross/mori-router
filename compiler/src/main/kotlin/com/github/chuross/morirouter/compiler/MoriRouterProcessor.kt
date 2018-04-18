13b968ea7625151412d61eba8538aa8ca0ebf872
chuross <azarasi.mo.mo@gmail.com>
2018-04-18 12:04:26 +0900

    fix support source version

--- compiler/src/main/kotlin/com/github/chuross/morirouter/compiler/MoriRouterProcessor.kt
+++ compiler/src/main/kotlin/com/github/chuross/morirouter/compiler/MoriRouterProcessor.kt
@@ -17,8 +17,6 @@ import javax.annotation.processing.Messager
 import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
-import javax.annotation.processing.SupportedAnnotationTypes
-import javax.annotation.processing.SupportedSourceVersion
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
 import javax.lang.model.util.Elements
@@ -40,7 +38,7 @@ class MoriRouterProcessor : AbstractProcessor() {
     }
 
     override fun getSupportedSourceVersion(): SourceVersion {
-        return SourceVersion.RELEASE_8
+        return SourceVersion.latestSupported()
     }
 
     override fun getSupportedAnnotationTypes(): MutableSet<String> {
