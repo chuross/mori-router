7c1b57f774f2cad3b9f9a820b1e16cade5cb8f15
chuross <azarasi.mo.mo@gmail.com>
2018-04-25 17:03:55 +0900

    update sample code

--- app/src/main/kotlin/com/github/chuross/morirouter/ListFragment.kt
+++ app/src/main/kotlin/com/github/chuross/morirouter/ListFragment.kt
@@ -1,6 +1,7 @@
 package com.github.chuross.morirouter
 
 import android.os.Bundle
+import android.support.v4.view.ViewCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.github.chuross.morirouter.annotation.Argument
@@ -30,11 +31,14 @@ class ListFragment : BaseFragment<FragmentListBinding>() {
         val context = context ?: return
 
         binding.list.layoutManager = LinearLayoutManager(context)
-        binding.list.adapter = ListItemAdapter(context, hashCode().toString()).also {
+        binding.list.adapter = ListItemAdapter(context).also {
             val imageUrls = arrayListOf(
                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzYOp3yPAai29yvQV91Gf5P4bsnzaiwU7B3mdHg2dVpRHLDZOK",
                     "http://yamura-yasuke.club/yamura/wp-content/uploads/2016/07/himawari20160719.jpg"
             )
+            it.onBindThumbnailTransitionName = { position, imageView ->
+                ViewCompat.setTransitionName(imageView, "shared_view_${hashCode()}_$position")
+            }
             it.addAll(imageUrls.toList())
             it.setOnItemClickListener { holder, _, url ->
                 holder.let { it as? BindingViewHolder<*> }
--- app/src/main/kotlin/com/github/chuross/morirouter/ListItemAdapter.kt
+++ app/src/main/kotlin/com/github/chuross/morirouter/ListItemAdapter.kt
@@ -1,14 +1,16 @@
 package com.github.chuross.morirouter
 
 import android.content.Context
-import android.support.v4.view.ViewCompat
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.github.chuross.morirouter.databinding.ViewListItemBinding
 import com.github.chuross.recyclerviewadapters.ItemAdapter
 import com.github.chuross.recyclerviewadapters.databinding.BindingViewHolder
 
-class ListItemAdapter(context: Context, private val identity: String) : ItemAdapter<String, BindingViewHolder<ViewListItemBinding>>(context) {
+class ListItemAdapter(context: Context) : ItemAdapter<String, BindingViewHolder<ViewListItemBinding>>(context) {
+
+    var onBindThumbnailTransitionName: ((Int, ImageView) -> Unit)? = null
 
     override fun getAdapterId(): Int = R.layout.view_list_item
 
@@ -17,9 +19,7 @@ class ListItemAdapter(context: Context, private val identity: String) : ItemAdap
     }
 
     override fun onBindViewHolder(holder: BindingViewHolder<ViewListItemBinding>, position: Int) {
-        holder.binding.thumbnailImage.also {
-            ViewCompat.setTransitionName(it, String.format("shared_view_%s_%d_%d", identity, position, it.id))
-        }
+        onBindThumbnailTransitionName?.invoke(position, holder.binding.thumbnailImage)
 
         holder.binding.imageUrl = get(position)
         holder.binding.index = position
