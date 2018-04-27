117f65fa1231b8f2680eb10f6a711345c84c537c
chuross <azarasi.mo.mo@gmail.com>
2018-04-27 19:34:28 +0900

    update sample code

--- app/src/main/kotlin/com/github/chuross/morirouter/util/Data.kt
+++ app/src/main/kotlin/com/github/chuross/morirouter/util/Data.kt
@@ -3,6 +3,13 @@ package com.github.chuross.morirouter.util
 object Data {
 
     val LIST_DATA: List<String> = listOf(
-            "http://yamura-yasuke.club/yamura/wp-content/uploads/2016/07/himawari20160719.jpg"
+            "https://photohito.k-img.com/uploads/photo91/user90548/3/2/327f5937df9dfeeef0888887cbbf53db/327f5937df9dfeeef0888887cbbf53db_m.jpg",
+            "https://photohito.k-img.com/uploads/photo96/user95786/8/e/8ee3dfc05598a8059a28c354dbf35de9/8ee3dfc05598a8059a28c354dbf35de9_m.jpg",
+            "https://photohito.k-img.com/uploads/photo122/user121059/d/d/dd95a4f46250fc3931e3038546163552/dd95a4f46250fc3931e3038546163552_m.jpg",
+            "https://photohito.k-img.com/uploads/photo58/user57948/6/6/66d03715cb9d9c65f86ed16ffc9459a3/66d03715cb9d9c65f86ed16ffc9459a3_m.jpg",
+            "https://photohito.k-img.com/uploads/photo103/user102779/7/0/706ac6983cd9759bf8d771ec65df9a9c/706ac6983cd9759bf8d771ec65df9a9c_m.jpg",
+            "https://photohito.k-img.com/uploads/photo65/user64489/0/2/0228ce801aaf3672ff6ca787aeae9bda/0228ce801aaf3672ff6ca787aeae9bda_m.jpg",
+            "https://photohito.k-img.com/uploads/photo42/user41578/0/5/0561d10749b1c460777a43878250ba02/0561d10749b1c460777a43878250ba02_m.jpg",
+            "https://photohito.k-img.com/uploads/photo86/user85262/b/9/b9a404d50178c6a992878805ef9b54b3/b9a404d50178c6a992878805ef9b54b3_m.jpg"
     )
 }
